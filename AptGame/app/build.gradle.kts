2485a06e19883c4fd4f360b56d8e5fb15fe4c5f5
junyong <junyong008@gmail.com>
2024-11-22 16:48:35 +0900

    build: Java와 Kotlin 버전 11로 통일 및 buildConfig 활성화

--- AptGame/app/build.gradle.kts
+++ AptGame/app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
         jvmTarget = "11"
@@ -39,6 +39,7 @@ android {
 
     buildFeatures {
         dataBinding = true
+        buildConfig = true
     }
 }
 
