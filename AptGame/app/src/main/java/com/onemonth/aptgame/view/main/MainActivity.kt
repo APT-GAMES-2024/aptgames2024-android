1a207925f0a2323cf675cbe4a6533b40aaef7a0b
4538cgy <aptgames2024@gmail.com>
2024-11-24 15:35:30 +0900

    json 추가 및 Main Activity

---
+++ AptGame/app/src/main/java/com/onemonth/aptgame/view/main/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.onemonth.aptgame.view.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.onemonth.aptgame.R
+import com.onemonth.aptgame.databinding.ActivityMainBinding
+import com.onemonth.aptgame.view.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
