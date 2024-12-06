8d0c9aa24886ad2f888a8bd1ca4381df008f8ee8
APT-GAMES-2024 <aptgames2024@gmail.com>
2024-12-06 13:40:29 +0900

    Revert "Feat/result 결과 화면"

--- AptGame/app/src/main/java/com/onemonth/aptgame/model/ExampleModel.kt
+++
@@ -1,6 +0,0 @@
-package com.onemonth.aptgame.model
-
-data class ExampleModel(
-    val name: String? = null,
-    val id: Int? = null
-)
--- AptGame/app/src/main/java/com/onemonth/aptgame/model/Reward.kt
+++
@@ -1,6 +0,0 @@
-package com.onemonth.aptgame.model
-
-data class Reward(
-    val points: Int,
-    val apartmentType: ApartmentType? = null,
-)
